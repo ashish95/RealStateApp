045ed68ff80490e471777c70908d388c2b47f59c
Sachin Sharma <sachinsharma.bokaro@gmail.com>
2019-10-08 23:14:02 +0530

    Set up search page

--- listings/views.py
+++ listings/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,get_object_or_404
 from django.core.paginator import EmptyPage,Paginator,PageNotAnInteger
 from .models import Listing
 from realtors.models import Realtor
+from .choices import state_choices,bedroom_choices,price_choices
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published = True)
@@ -28,4 +29,13 @@ def listing(request,listing_id):
     return render(request, 'listings/listing.html',context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    context = {
+        'state_choices':state_choices,
+        'bedroom_choices':bedroom_choices,
+        'price_choices':price_choices,
+        'listings' : queryset_list 
+    }
+    return render(request, 'listings/search.html',context)
