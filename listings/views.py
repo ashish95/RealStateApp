db06c98e358c57082a5066d3329a3e676a80514a
Sachin Sharma <sachinsharma.bokaro@gmail.com>
2019-10-08 18:50:00 +0530

    Single listing logic and making pages dynamic

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.core.paginator import EmptyPage,Paginator,PageNotAnInteger
+from .models import Listing
+from realtors.models import Realtor
 
-def listings(request):
-    return render(request,'listings/listings.html')
+def index(request):
+    listings = Listing.objects.order_by('-list_date').filter(is_published = True)
+    paginator = Paginator(listings,6)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+    
+    context = {
+        'listings':paged_listings 
+    }
 
-def listing(request):
-    return render(request,'listings/listing.html')
+    return render(request, 'listings/listings.html',context)
+
+def listing(request,listing_id):
+
+    listing = get_object_or_404(Listing,pk=listing_id)
+
+    # mvp_realtor = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'listing':listing
+    }
+
+    return render(request, 'listings/listing.html',context)
 
 def search(request):
-    return render(request,'listings/search.html')
+    return render(request, 'listings/search.html')
