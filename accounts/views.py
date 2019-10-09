7d30b161757794a04bd5ed0f2bf61cbf0226b344
Sachin Sharma <sachinsharma.bokaro@gmail.com>
2019-10-09 18:19:41 +0530

    inluded messages app functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
+def login(request):
+    return render(request,'accounts/login.html')
+
+def register(request):
+    if request.method == "POST":
+        messages.error(request,'Testing error message')
+        return redirect('register')
+    else:
+        return render(request,'accounts/register.html')
+
+def logout(request):
+    return redirect('index')
+
+def dashboard(request):
+    return render(request,'accounts/dashboard.html')
